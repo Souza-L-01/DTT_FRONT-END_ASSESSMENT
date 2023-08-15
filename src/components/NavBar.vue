7ee7f9f5ef752fa97909b78adf7ea6e28bc8892f
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-15 10:14:55 +0200

    sort files/ update yarn to npm

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <img src="../src/assets/images/DTT_logo_black.svg" alt="DTT logo" click="$router.push('/')"/>
+    <router-link >Houses</router-link>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your New Life"/>
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld,
+//   },
+// };
+// </script>
