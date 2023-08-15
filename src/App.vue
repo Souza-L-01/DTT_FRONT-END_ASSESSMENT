802b37d0aaf47740672eecc187bea6e91cea16cb
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-15 15:28:02 +0200

    start NavBar component

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="normal fixed-nav-bar">
+    <NavBar />
+  </nav>
+  <router-view class="forFixed" />
+  <nav class="sticky">
+    <NavBar />
+  </nav>
+</template>
+
+<script setup>
+import NavBar from './components/NavBar.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: rgb(233, 232, 232);
+  min-height: 100vh;
+}
+.fixed-nav-bar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+  width: 100%;
+}
+.forFixed {
+  margin-top: 65px;
+}
+
+.sticky {
+  display: none;
+}
+
+@media only screen and (max-width: 768px) {
+  .normal {
+    display: none;
+  }
+  .sticky {
+    display: block;
+    position: -webkit-sticky;
+    position: sticky;
+    bottom: 0;
+    align-self: flex-start;
+  }
+}
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,65 @@
 <template>
-  <div id="nav">
-    <img src="../src/assets/images/DTT_logo_black.svg" alt="DTT logo" click="$router.push('/')"/>
-    <router-link >Houses</router-link>
+  <div class="main">
+    <img alt="DTT logo" src="../assets/images/dtt_logo_black.svg" class="hideInMobile" />
 
+    <router-link to="/">
+      <p class="hideInMobile">Houses</p>
+      <span class="material-symbols-outlined showInMobile"> home </span>
+    </router-link>
+
+    <router-link to="/about">
+      <p class="hideInMobile">About</p>
+
+      <span class="material-symbols-outlined showInMobile"> info </span>
+    </router-link>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 1.2em;
+  padding: 0% 15%;
+  background-color: white;
+}
 
+img {
+  height: 7em;
+  width: 7em;
+}
+nav {
+  padding: 20px;
+}
+
+nav a {
+  color: #1e1519;
+  margin: 0% 5%;
+  text-decoration: none;
+}
+
+nav a.router-link-exact-active {
+  font-weight: bolder;
+}
+.showInMobile {
+  display: none;
+}
+.hideInMobile {
+  display: block;
+}
+@media only screen and (max-width: 768px) {
+  .showInMobile {
+    display: block;
+  }
+  .hideInMobile {
+    display: none;
+  }
+  .main {
+    justify-content: space-around;
+  }
+}
 </style>
